014da26356aa2d3ed9dce4ac8ad96821990d377f
Swapnil(Technology-Gurgaon) <swapnil@hindustantimes.com>
2025-12-12 15:44:08 +0530

    fix: populate using the json files
    Signed-off-by: Swapnil(Technology-Gurgaon) <swapnil@hindustantimes.com>

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      <p className="text-sm">
+        Aspiring developer with a knack for frontend magic. I{`'`}m
+        well-versed in JavaScript, TypeScript, React, and Node.js, and I{`'`}m
+        eager to kickstart my career in a dynamic environment.{" "}
+      </p>
+    </div>
+  );
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,63 +1,28 @@
-export default function Page() {
-  const pinnedRepositories = [
-    {
-      id: "1",
-      name: "Personal Portfolio",
-      url: "https://github.com/dev-swapnilx/Portfolio_NextJS.git",
-      description: "Personal Portfolio Made using NextJS",
-    },
-    {
-      id: "2",
-      name: "OmniBoard: A Multi-Gaming board for Specially Abled",
-      url: "https://github.com/dev-swapnilx/smart-board.git",
-      description:
-        "Smart, motorized board games with real-time web interface for immersive play",
-    },
-    {
-      id: "3",
-      name: "SafeStep: Early Fall Prediction for Senior Citizens with IOT",
-      url: "https://github.com/dev-swapnilx/SafeStep.git",
-      description:
-        "IoT device with ML for fall prediction, triggering alarms and inflating jackets for elder safety",
-    },
-    {
-      id: "4",
-      name: "Argri-Site: Weed detection and removal drone",
-      url: "https://github.com/dev-swapnilx/Argri-Site.git",
-      description:
-        "Autonomous drone for weed detection using Raspberry Pi and ML, aiding precision herbicide application",
-    },
-    {
-      id: "5",
-      name: "CogniCodeML: v1.0 - C++ Consciousness Unleashed",
-      url: "https://github.com/dev-swapnilx/Machine_Learning",
-      description:
-        "A C++ repository housing kNN, K-Means, and Neural Network implementations on MNIST Handwriting and Iris Datasets, optimized for efficiency and clarity.",
-    },
-    
-  ];
+import { getProjects } from '@/utils/importProjects';
+
+export default async function Page() {
+  const pinnedRepositories = await getProjects();
+  
 
   return (
-    <ul className="space-y-4">
-      {pinnedRepositories.map((repo) => (
-        <li key={repo.id}>
-          <a
-            className="bg-transparent border border-[#ecebeb] hover:border-[#999] dark:border-[#333] hover:dark:bg-[#ffffff05]
-              transition-colors
-              p-2 flex flex-col space-y-1.5 !no-underline rounded-md"
-            rel="noopener noreferrer"
-            target="_blank"
-            href={repo.url}
-          >
-            <div>
-              <span className="text-sm underline underline-offset-4 ">
-                {repo.name}
-              </span>
-            </div>
-            <span className="text-sm text-neutral-500">{repo.description}</span>
-          </a>
-        </li>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      {pinnedRepositories?.map((repo) => (
+        <a
+          key={repo.id}
+          className="bg-transparent border border-[#ecebeb] hover:border-[#999] dark:border-[#333] hover:dark:bg-[#ffffff05]
+            transition-colors p-4 flex flex-col space-y-2 !no-underline rounded-md h-full"
+          rel="noopener noreferrer"
+          target="_blank"
+          href={repo.url}
+        >
+          <div className="flex-1">
+            <h3 className="text-base font-medium underline underline-offset-4">
+              {repo.name}
+            </h3>
+            <p className="mt-2 text-sm text-neutral-500">{repo.description}</p>
+          </div>
+        </a>
       ))}
-    </ul>
+    </div>
   );
 }
